63655834524d6bc94a3e0c0742804aba3432b16d
YusufZartit <gonenc@dakikyazilim.com>
2023-07-19 12:11:18 +0300

    fix: add "findAllByIdIn" and "findByExhibitId" function to ExhibitionItemRepository.

--- src/main/java/com/iotiq/application/repository/ExhibitionItemRepository.java
+++ src/main/java/com/iotiq/application/repository/ExhibitionItemRepository.java
@@ -4,7 +4,11 @@ import com.iotiq.application.domain.ExhibitionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ExhibitionItemRepository extends JpaRepository<ExhibitionItem, UUID>, JpaSpecificationExecutor<ExhibitionItem> {
+    List<ExhibitionItem> findAllByIdIn(List<UUID> ids);
+    List<ExhibitionItem> findByExhibitId(UUID exhibitId);
+
 }
