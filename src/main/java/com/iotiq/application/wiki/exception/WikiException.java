da91e4ca8000570cd6020816f7295aab0e1a219c
inci <inci@iotiq.net>
2023-07-20 11:26:04 +0300

    remove comment

--- src/main/java/com/iotiq/application/wiki/exception/WikiException.java
+++ src/main/java/com/iotiq/application/wiki/exception/WikiException.java
@@ -8,6 +8,5 @@ import java.util.Collections;
 public class WikiException extends ApplicationException {
     public WikiException() {
         super(HttpStatus.INTERNAL_SERVER_ERROR, "wikiException", Collections.emptyList());
-//        sout
     }
 }
