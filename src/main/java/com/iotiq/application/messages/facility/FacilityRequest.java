52844f1c1f98e3e1b9c91bf646fd1b08ea62a094
YusufZartit <gonenc@dakikyazilim.com>
2023-07-24 12:42:47 +0300

    fix: Replace the condition with the validation annotations.

--- src/main/java/com/iotiq/application/messages/facility/FacilityRequest.java
+++ src/main/java/com/iotiq/application/messages/facility/FacilityRequest.java
@@ -1,19 +1,24 @@
 package com.iotiq.application.messages.facility;
 
-import com.iotiq.commons.exceptions.RequiredFieldMissingException;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalTime;
 
-public record FacilityRequest(@NotNull @NotEmpty String openingTime, @NotNull @NotEmpty String closingTime) {
-    public FacilityRequest {
-        LocalTime openingTimeParsed = LocalTime.parse(openingTime);
-        LocalTime closingTimeParsed = LocalTime.parse(closingTime);
-        if (openingTimeParsed.isAfter(closingTimeParsed)) {
-            throw new RequiredFieldMissingException("The opening time must be before the closing time.");
-        }
-    }
+
+
+public record FacilityRequest(
+        @NotNull(message = "Opening time cannot be null.")
+        @NotEmpty(message = "Opening time cannot be empty.")
+        @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Invalid opening time format. Use HH:mm.")
+        String openingTime,
+
+        @NotNull(message = "Closing time cannot be null.")
+        @NotEmpty(message = "Closing time cannot be empty.")
+        @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Invalid closing time format. Use HH:mm.")
+        String closingTime
+) {
 
     public LocalTime getOpeningTimeAsLocalTime() {
         return LocalTime.parse(openingTime);
@@ -22,6 +27,5 @@ public record FacilityRequest(@NotNull @NotEmpty String openingTime, @NotNull @N
     public LocalTime getClosingTimeAsLocalTime() {
         return LocalTime.parse(closingTime);
     }
-    
 }
 
