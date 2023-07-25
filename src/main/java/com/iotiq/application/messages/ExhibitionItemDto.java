598c6c13cc28703998bd8bb359f2db14d69bb880
Rafeh <rafeh.12102@gmail.com>
2023-07-25 07:50:42 +0300

    requested changes implemented

--- src/main/java/com/iotiq/application/messages/ExhibitionItemDto.java
+++ src/main/java/com/iotiq/application/messages/ExhibitionItemDto.java
@@ -4,9 +4,9 @@ import com.iotiq.application.domain.ExhibitionItem;
 
 import java.util.UUID;
 
-public record ExhibitionItemDto(UUID id, String wikiId, String path, String title, String description, String content) {
+public record ExhibitionItemDto(UUID id, String wikiId, String path, String title) {
     public static ExhibitionItemDto of(ExhibitionItem exhibitionItem) {
         return new ExhibitionItemDto(exhibitionItem.getId(), exhibitionItem.getWikiId(), exhibitionItem.getPath(),
-                exhibitionItem.getTitle(),  exhibitionItem.getDescription(),  exhibitionItem.getContent());
+                exhibitionItem.getTitle());
     }
 }
