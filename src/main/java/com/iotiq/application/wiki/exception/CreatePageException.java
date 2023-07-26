1ada4493739883f24c4e0ad9fa47aa506364239b
Rafeh <rafeh.12102@gmail.com>
2023-07-26 16:18:25 +0300

    CreatePageException implemented but returns null

---
+++ src/main/java/com/iotiq/application/wiki/exception/CreatePageException.java
@@ -0,0 +1,12 @@
+package com.iotiq.application.wiki.exception;
+
+import com.iotiq.commons.exceptions.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+import java.util.Collections;
+
+public class CreatePageException extends ApplicationException {
+    public CreatePageException() {
+        super(HttpStatus.INTERNAL_SERVER_ERROR, "createPageException", Collections.emptyList());
+    }
+}
