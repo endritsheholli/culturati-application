fc18efd162ce8f297e60a9dd6897b42913c1adf0
YusufZartit <gonenc@dakikyazilim.com>
2023-07-27 15:13:57 +0300

    fix: add "findAllByIdIn" method

--- src/main/java/com/iotiq/application/repository/NavPointRepository.java
+++ src/main/java/com/iotiq/application/repository/NavPointRepository.java
@@ -3,7 +3,10 @@ package com.iotiq.application.repository;
 import com.iotiq.application.domain.NavPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public interface NavPointRepository extends JpaRepository<NavPoint, UUID> {
+    Set<NavPoint> findAllByIdIn(List<UUID> ids);
 }
