31e4eb10a0e0be7a408d63a8f607552c2f007cc9
YusufZartit <gonenc@dakikyazilim.com>
2023-07-28 09:36:37 +0300

    fix: Update NavPointDto to use UUID Set for children.

--- src/main/java/com/iotiq/application/messages/navpoint/NavPointDto.java
+++ src/main/java/com/iotiq/application/messages/navpoint/NavPointDto.java
@@ -4,23 +4,25 @@ import com.iotiq.application.domain.Exhibit;
 import com.iotiq.application.domain.ExhibitionItem;
 import com.iotiq.application.domain.FacilityOrEstablishment;
 import com.iotiq.application.domain.NavPoint;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public record NavPointDto(UUID id,
                           String mapId,
                           Set<ExhibitionItem> exhibitionItems,
                           Set<Exhibit> exhibits,
                           Set<FacilityOrEstablishment> facilities,
-                          Set<NavPoint> children) {
+                          Set<UUID> children) {
 
     public static NavPointDto of(NavPoint navPoint) {
         return new NavPointDto(navPoint.getId(),
                 navPoint.getMapId(),
                 navPoint.getExhibitionItems(), 
                 navPoint.getExhibits(), 
-                navPoint.getFacilities(), 
-                navPoint.getChildren());
+                navPoint.getFacilities(),
+                navPoint.getChildren().stream().map(AbstractPersistable::getId).collect(Collectors.toSet()));
     }
 }
