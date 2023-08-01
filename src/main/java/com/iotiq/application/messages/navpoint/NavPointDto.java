c2cb4e72901161c7faf31d19e23016d423c82902
YusufZartit <gonenc@dakikyazilim.com>
2023-08-01 16:33:48 +0300

    refactor: Create dedicated DTOs for NavPoint fields

--- src/main/java/com/iotiq/application/messages/navpoint/NavPointDto.java
+++ src/main/java/com/iotiq/application/messages/navpoint/NavPointDto.java
@@ -1,9 +1,9 @@
 package com.iotiq.application.messages.navpoint;
 
-import com.iotiq.application.domain.Exhibit;
-import com.iotiq.application.domain.ExhibitionItem;
-import com.iotiq.application.domain.Facility;
 import com.iotiq.application.domain.NavPoint;
+import com.iotiq.application.messages.ExhibitionItemDto;
+import com.iotiq.application.messages.exhibit.ExhibitDto;
+import com.iotiq.application.messages.facility.FacilityDto;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import java.util.Set;
@@ -12,17 +12,17 @@ import java.util.stream.Collectors;
 
 public record NavPointDto(UUID id,
                           String mapId,
-                          Set<ExhibitionItem> exhibitionItems,
-                          Set<Exhibit> exhibits,
-                          Set<Facility> facilities,
+                          Set<ExhibitionItemDto> exhibitionItems,
+                          Set<ExhibitDto> exhibits,
+                          Set<FacilityDto> facilities,
                           Set<UUID> children) {
 
     public static NavPointDto of(NavPoint navPoint) {
         return new NavPointDto(navPoint.getId(),
                 navPoint.getMapId(),
-                navPoint.getExhibitionItems(),
-                navPoint.getExhibits(),
-                navPoint.getFacilities(),
+                navPoint.getExhibitionItems().stream().map(ExhibitionItemDto::of).collect(Collectors.toSet()),
+                navPoint.getExhibits().stream().map(ExhibitDto::of).collect(Collectors.toSet()),
+                navPoint.getFacilities().stream().map(FacilityDto::of).collect(Collectors.toSet()),
                 navPoint.getChildren().stream().map(AbstractPersistable::getId).collect(Collectors.toSet()));
     }
 }
