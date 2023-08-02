536e67347f8b35c34c3205d9ef812c4abe707f50
YusufZartit <gonenc@dakikyazilim.com>
2023-08-02 10:26:41 +0300

    fix: refactor exception of GeoJSON methods.

--- src/main/java/com/iotiq/application/controller/GeoJSONController.java
+++ src/main/java/com/iotiq/application/controller/GeoJSONController.java
@@ -11,8 +11,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @Tag(name = "Geospatial", description = "Geospatial API")
 @RestController
@@ -21,43 +19,29 @@ import java.io.IOException;
 public class GeoJSONController {
 
     private final GeoJSONService geoJSONService;
-    
 
     @PostMapping("/{tenantName}")
     @PreAuthorize("hasAuthority(@GeoJSONAuth.CREATE)")
-    public ResponseEntity<String> uploadGeoJSON(@PathVariable String tenantName, @RequestParam("file") MultipartFile file) {
-        try {
-            String message = geoJSONService.saveGeoJSONFile(tenantName, file);
-
-            return ResponseEntity.ok(message);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File upload error: " + e.getMessage());
-        }
+    public ResponseEntity<Void> uploadGeoJSON(@PathVariable String tenantName, @RequestParam("file") MultipartFile file) throws Exception {
+        geoJSONService.saveGeoJSONFile(tenantName, file);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{tenantName}")
     @PreAuthorize("hasAuthority(@GeoJSONAuth.DELETE)")
-    public ResponseEntity<String> deleteGeoJSON(@PathVariable String tenantName) {
-        try {
-            String message = geoJSONService.deleteGeoJSONFile(tenantName);
+    public ResponseEntity<Void> deleteGeoJSON(@PathVariable String tenantName) {
+        geoJSONService.deleteGeoJSONFile(tenantName);
 
-            return ResponseEntity.ok(message);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File deletion error: " + e.getMessage());
-        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{tenantName}")
     @PreAuthorize("hasAuthority(@GeoJSONAuth.VIEW)")
     public ResponseEntity<Resource> getGeoJSON(@PathVariable String tenantName) {
-        try {
-            Resource resource = geoJSONService.getGeoJSONFile(tenantName);
+        Resource resource = geoJSONService.getGeoJSONFile(tenantName);
 
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + tenantName + ".json\"")
-                    .body(resource);
-        } catch (FileNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + tenantName + ".json\"")
+                .body(resource);
     }
 }
--- src/main/java/com/iotiq/application/service/GeoJSONService.java
+++ src/main/java/com/iotiq/application/service/GeoJSONService.java
@@ -1,12 +1,13 @@
 package com.iotiq.application.service;
 
+import com.iotiq.application.exception.GeoJsonFileNotFoundException;
+import com.iotiq.application.exception.GeoJsonFileOperationException;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,38 +18,45 @@ public class GeoJSONService {
 
     private static final String FILE_DIRECTORY = System.getProperty("user.dir") + "/application/src/main/resources/files/";
 
-    public String saveGeoJSONFile(String tenantName, MultipartFile file) throws IOException {
+    public String saveGeoJSONFile(String tenantName, MultipartFile file) throws GeoJsonFileOperationException {
         String fileName = tenantName + ".geojson";
 
-        File directory = new File(FILE_DIRECTORY);
-        if (!directory.exists()) {
-            directory.mkdirs();
+        try {
+            File directory = new File(FILE_DIRECTORY);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            File destFile = new File(directory, fileName);
+            file.transferTo(destFile);
+
+            return destFile.getAbsolutePath();
+        } catch (IOException exp) {
+            throw new GeoJsonFileOperationException("file_save_error", "Error while saving the GeoJSON file.", exp);
         }
-
-        File destFile = new File(directory, fileName);
-        file.transferTo(destFile);
-
-        return "The file has been uploaded successfully.";
     }
 
-    public String deleteGeoJSONFile(String tenantName) throws IOException {
+
+    public void deleteGeoJSONFile(String tenantName) throws GeoJsonFileOperationException {
         String fileName = tenantName + ".geojson";
 
         Path filePath = Paths.get(FILE_DIRECTORY, fileName);
-
-        if (Files.exists(filePath)) {
-            Files.delete(filePath);
+        try {
+            if (Files.exists(filePath)) {
+                Files.delete(filePath);
+            }
+        } catch (IOException exp) {
+            throw new GeoJsonFileOperationException("file_delete_error", "Error while deleting the GeoJSON file.", exp);
         }
-        return "The file was successfully deleted.";
     }
 
-    public Resource getGeoJSONFile(String tenantName) throws FileNotFoundException {
+    public Resource getGeoJSONFile(String tenantName) throws GeoJsonFileNotFoundException {
         String fileName = tenantName + ".geojson";
         Path filePath = Paths.get(FILE_DIRECTORY, fileName);
 
         Resource resource = new FileSystemResource(filePath.toFile());
         if (!resource.exists()) {
-            throw new FileNotFoundException("File not found.");
+            throw new GeoJsonFileNotFoundException("File not found.");
         }
 
         return resource;
