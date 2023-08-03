fa6a3bbfc2d5407a4e41beed6194af837b64c0d7
YusufZartit <gonenc@dakikyazilim.com>
2023-08-03 09:33:38 +0300

    fix: add coordinates to `ExhibitionItemDto` class.

--- src/main/java/com/iotiq/application/messages/ExhibitionItemDto.java
+++ src/main/java/com/iotiq/application/messages/ExhibitionItemDto.java
@@ -4,9 +4,9 @@ import com.iotiq.application.domain.ExhibitionItem;
 
 import java.util.UUID;
 
-public record ExhibitionItemDto(UUID id, String wikiId, String path, String title) {
+public record ExhibitionItemDto(UUID id, String wikiId, String path, String title, Double latitude, Double longitude) {
     public static ExhibitionItemDto of(ExhibitionItem exhibitionItem) {
         return new ExhibitionItemDto(exhibitionItem.getId(), exhibitionItem.getWikiId(), exhibitionItem.getPath(),
-                exhibitionItem.getTitle());
+                exhibitionItem.getTitle(), exhibitionItem.getLatitude(), exhibitionItem.getLongitude());
     }
 }
