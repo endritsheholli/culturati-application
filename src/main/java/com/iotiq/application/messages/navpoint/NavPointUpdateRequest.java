4576e96b2331734cff5e5880e66d6e05c99fa80a
YusufZartit <gonenc@dakikyazilim.com>
2023-08-03 15:08:57 +0300

    fix: add coordinates to `NavPointUpdateRequest` class.

--- src/main/java/com/iotiq/application/messages/navpoint/NavPointUpdateRequest.java
+++ src/main/java/com/iotiq/application/messages/navpoint/NavPointUpdateRequest.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.UUID;
 
 public record NavPointUpdateRequest(String mapId,
+                                    double latitude,
+                                    double longitude,
                                     @NotNull List<UUID> facilityIds,
                                     @NotNull List<UUID> exhibitionItemIds,
                                     @NotNull List<UUID> exhibitIds,
