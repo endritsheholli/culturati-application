061f3ccc312ced4478029093febfd5556b02fe27
YusufZartit <gonenc@dakikyazilim.com>
2023-08-04 14:45:30 +0300

    fix: add line breaks.

--- src/main/java/com/iotiq/application/messages/ExhibitionItemDto.java
+++ src/main/java/com/iotiq/application/messages/ExhibitionItemDto.java
@@ -7,7 +7,11 @@ import java.util.UUID;
 
 public record ExhibitionItemDto(UUID id, String wikiId, String path, String title, Location location) {
     public static ExhibitionItemDto of(ExhibitionItem exhibitionItem) {
-        return new ExhibitionItemDto(exhibitionItem.getId(), exhibitionItem.getWikiId(), exhibitionItem.getPath(),
-                exhibitionItem.getTitle(), exhibitionItem.getLocation());
+        return new ExhibitionItemDto(
+                exhibitionItem.getId(),
+                exhibitionItem.getWikiId(),
+                exhibitionItem.getPath(),
+                exhibitionItem.getTitle(),
+                exhibitionItem.getLocation());
     }
 }
