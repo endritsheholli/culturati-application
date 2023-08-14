07a9247c4a252bc0b8a203493de2467fbd853233
YusufZartit <gonenc@dakikyazilim.com>
2023-08-14 09:56:49 +0300

    Refactor GeoJSON File Handling and Extension Validation.

--- src/main/java/com/iotiq/application/service/GeoSpatialDataService.java
+++ src/main/java/com/iotiq/application/service/GeoSpatialDataService.java
@@ -3,6 +3,8 @@ package com.iotiq.application.service;
 import com.iotiq.application.config.TenantContext;
 import com.iotiq.application.exception.GeoJsonFileOperationException;
 import com.iotiq.commons.exceptions.EntityNotFoundException;
+import com.iotiq.application.util.FileUtil;
+import com.iotiq.commons.exceptions.RequiredFieldMissingException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -11,15 +13,25 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class GeoSpatialDataService {
 
     @Value("${mapFileDirectory}")
     private Resource fileDirectory;
+    private final List<String> allowedExtensions = Arrays.asList(".json", ".geojson");
 
     public void saveGeoJSONFile(MultipartFile file) {
         try {
+            String filename = file.getOriginalFilename();
+            if (filename != null) {
+                String fileExtension = FileUtil.getFileExtension(filename);
+                if (!allowedExtensions.contains(fileExtension)) {
+                    throw new RequiredFieldMissingException("allowedExtensions");
+                }
+            }
             File directory = createGeoJsonFile();
             file.transferTo(directory);
 
@@ -57,6 +69,9 @@ public class GeoSpatialDataService {
     private File getGeoJsonFile() {
         try {
             String tenantName = TenantContext.getCurrentTenant();
+            if(tenantName == null || tenantName.isEmpty()){
+                throw new RequiredFieldMissingException("tenantName");
+            }
             String fileName = tenantName + ".geojson";
             return new File(fileDirectory.getFile().getAbsolutePath(), fileName);
         } catch (IOException exp) {
