195c78396a354967b21d114949b2fe2dee3849a2
YusufZartit <gonenc@dakikyazilim.com>
2023-08-23 16:57:22 +0300

    Use the create method in UserService.

--- src/main/java/com/iotiq/application/service/RegistrationService.java
+++ src/main/java/com/iotiq/application/service/RegistrationService.java
@@ -2,10 +2,8 @@ package com.iotiq.application.service;
 
 import com.iotiq.application.domain.Role;
 import com.iotiq.application.messages.register.RegistrationRequest;
-import com.iotiq.commons.util.PasswordUtil;
-import com.iotiq.user.domain.User;
-import com.iotiq.user.exceptions.DuplicateUserDataException;
-import com.iotiq.user.internal.UserRepository;
+import com.iotiq.user.internal.UserService;
+import com.iotiq.user.messages.request.UserCreateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,27 +12,19 @@ import static com.iotiq.commons.util.NullHandlerUtil.setIfNotNull;
 @Service
 @RequiredArgsConstructor
 public class RegistrationService {
-    private final UserRepository userRepository;
-    private final PasswordUtil passwordUtil;
+    private final UserService userService;
 
     public void save(RegistrationRequest request) {
-        // Check if the username or email is already in use
-        if (userRepository.findByAccountInfoUsername(request.username()).isPresent()) {
-            throw new DuplicateUserDataException("username");
-        }
-        if (userRepository.findByPersonalInfoEmail(request.email()).isPresent()) {
-            throw new DuplicateUserDataException("email");
-        }
         
-        // Create a new user with VISITOR role
-        User user = new User();
-        setIfNotNull(user::setPassword, () -> passwordUtil.encode(request.password()), request.password());
-        user.setRole(Role.VISITOR); // Set the role to VISITOR
-        setIfNotNull(user::setUsername, request::username);
-        setIfNotNull(s -> user.getPersonalInfo().setFirstName(s), request::firstname);
-        setIfNotNull(s -> user.getPersonalInfo().setLastName(s), request::lastname);
-        setIfNotNull(s -> user.getPersonalInfo().setEmail(s), request::email);
+        // Create a new UserCreateDto with VISITOR role
+        UserCreateDto userCreateDto = new UserCreateDto();
+        setIfNotNull(userCreateDto::setPassword, request::password);
+        userCreateDto.setRole(Role.VISITOR); // Set the role to VISITOR
+        setIfNotNull(userCreateDto::setUsername, request::username);
+        setIfNotNull(userCreateDto::setFirstname, request::firstname);
+        setIfNotNull(userCreateDto::setLastname, request::lastname);
+        setIfNotNull(userCreateDto::setEmail, request::email);
 
-        userRepository.save(user);
+        userService.create(userCreateDto);
     }
 }
