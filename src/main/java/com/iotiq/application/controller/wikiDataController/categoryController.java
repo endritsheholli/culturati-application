7c4272e2ffe0a685913dc7c0d742fd48eedf4faf
YusufZartit <gonenc@dakikyazilim.com>
2023-09-04 11:53:47 +0300

    Add categoryController .

---
+++ src/main/java/com/iotiq/application/controller/wikiDataController/categoryController.java
@@ -0,0 +1,25 @@
+package com.iotiq.application.controller.wikiDataController;
+
+import com.iotiq.application.service.wikiDataService.CategoryService;
+import com.iotiq.application.wiki.messages.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category")
+@RequiredArgsConstructor
+public class categoryController {
+    private final CategoryService categoryService;
+    @GetMapping
+    @PreAuthorize("hasAuthority(@MuseumManagementAuth.VIEW)")
+    public ResponseEntity<List<CategoryResponse>> getAll(){
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
+    }
+}
