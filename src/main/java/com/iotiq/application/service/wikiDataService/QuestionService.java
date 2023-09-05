cb773e43616fe70461065ba248980cd609195861
YusufZartit <gonenc@dakikyazilim.com>
2023-09-05 16:21:07 +0300

    Add QuestionService.

---
+++ src/main/java/com/iotiq/application/service/wikiDataService/QuestionService.java
@@ -0,0 +1,38 @@
+package com.iotiq.application.service.wikiDataService;
+
+import com.iotiq.application.domain.Question;
+import com.iotiq.application.wiki.WikiDummyDataHolder;
+import com.iotiq.application.wiki.domain.QuestionDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    public List<Question> getQuestions(String categoryName, String difficultyLevel, int numberOfQuestions) {
+        List<QuestionDto> questionDtos = WikiDummyDataHolder.questions;
+        return convertQuestionDtosToQuestions(questionDtos);
+    }
+
+    public List<Question> convertQuestionDtosToQuestions(List<QuestionDto> questionDtos) {
+        return questionDtos.stream()
+                .map(dto -> {
+                    Question question = new Question();
+                    question.setTitle(dto.title());
+                    question.setHint(dto.hint());
+                    question.setQuestionType(dto.questionType());
+                    question.setOptions(dto.options());
+                    question.setRightAnswer(dto.rightAnswer());
+                    question.setPointAnswer(dto.pointAnswer());
+                    question.setPenalty(dto.penalty());
+                    question.setMoreInformationUrl(dto.moreInformationUrl());
+
+                    return question;
+                })
+                .collect(Collectors.toList());
+    }
+    
+}
