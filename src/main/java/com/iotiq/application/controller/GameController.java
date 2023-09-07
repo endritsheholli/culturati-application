f793bde9e8922452e0a5b256d55e2159a181b81a
YusufZartit <gonenc@dakikyazilim.com>
2023-09-07 09:53:02 +0300

    Add createGame endpoint.

---
+++ src/main/java/com/iotiq/application/controller/GameController.java
@@ -0,0 +1,26 @@
+package com.iotiq.application.controller;
+
+import com.iotiq.application.messages.game.GameRequest;
+import com.iotiq.application.service.GameService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Tag(name = "Game", description = "Games API")
+@RestController
+@RequestMapping("/api/v1/game")
+@RequiredArgsConstructor
+public class GameController {
+    private final GameService gameService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAuthority(@GameAuth.CREATE)")
+    public UUID createGame(@RequestBody GameRequest request) {
+        return gameService.createGame(request).getId();
+    }
+}
