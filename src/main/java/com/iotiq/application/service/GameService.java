9f4e97b2be13160086499d8f24bbc13c3e0569a5
YusufZartit <gonenc@dakikyazilim.com>
2023-09-10 12:33:41 +0300

    Set the difficulty level feature when creating the game.

--- src/main/java/com/iotiq/application/service/GameService.java
+++ src/main/java/com/iotiq/application/service/GameService.java
@@ -1,10 +1,11 @@
 package com.iotiq.application.service;
 
-import com.iotiq.application.domain.*;
+import com.iotiq.application.domain.Game;
+import com.iotiq.application.domain.GameStatus;
+import com.iotiq.application.domain.Gamer;
+import com.iotiq.application.domain.GamerGame;
 import com.iotiq.application.messages.game.CreateGameRequest;
 import com.iotiq.application.repository.GameRepository;
-import com.iotiq.application.repository.QuestionRepository;
-import com.iotiq.application.service.wikiDataService.QuestionService;
 import com.iotiq.user.domain.User;
 import com.iotiq.user.internal.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,18 +13,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
     private final GameRepository gameRepository;
-    private final QuestionService questionService;
-    private final QuestionRepository questionRepository;
     private final UserService userService;
     private final GamerService gamerService;
     private final GamerGameService gamerGameService;
-    private final GamerQuestionService gamerQuestionService;
 
     @Transactional
     public Game createGame(CreateGameRequest request) {
@@ -31,22 +28,15 @@ public class GameService {
         User user = userService.getCurrentUser();
 
         // TODO: Check Eligibility Before Creating a Game issue #82
-        
+
         // Get or create a Gamer entity associated with the user
         Gamer gamer = gamerService.getOrCreateGamerForUser(user);
 
-        // Retrieve a list of questions based on the request parameters
-        List<Question> questions = questionService.getQuestions(request.category(), request.difficultyLevel(), request.numberOfQuestions());
-        questionRepository.saveAll(questions);
-        
         // Create a Game entity
         Game game = mapRequestToEntity(request);
 
         // Create GamerGame for the gamer as CREATOR and establish bidirectional associations
         GamerGame gamerGame = gamerGameService.createGamerGameAndAssociateWithGameAndGamer(gamer, game);
-
-        // Create GamerQuestion for each question as PENDING and establish bidirectional associations
-        List<GamerQuestion> gamerQuestion = gamerQuestionService.createGamerQuestionListAndAssociateWithQuestions(questions, gamerGame);
 
         // Save the game
         return gameRepository.save(game);
@@ -56,6 +46,7 @@ public class GameService {
         Game game = new Game();
         game.setName(request.name());
         game.setGameType(request.gameType());
+        game.setDifficultyLevel(request.difficultyLevel());
         game.setStartTime(new Date());
         game.setStatus(GameStatus.NOT_STARTED);
         // You can uncomment the line below when the audioFileUrl feature is available
