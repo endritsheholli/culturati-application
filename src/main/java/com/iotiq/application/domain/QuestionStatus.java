bc6f3421e201de6fe0fce9e66af1de02261f89ab
YusufZartit <gonenc@dakikyazilim.com>
2023-09-10 12:26:09 +0300

    Remove classes related to "Question" for now (createGame).

--- src/main/java/com/iotiq/application/domain/QuestionStatus.java
+++
@@ -1,8 +0,0 @@
-package com.iotiq.application.domain;
-
-public enum QuestionStatus {
-    PENDING,        // The question is part of the game but hasn't been answered yet
-    ANSWERED,       // The question has been answered
-    SKIPPED,        // The question has been skipped
-    EXPIRED         // The question has expired due to time limit
-}
