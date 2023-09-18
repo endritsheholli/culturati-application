de56728f8383e497fed8ce8a430844f1b0f26673
Oguz Kurt <oguz@iotiq.net>
2023-09-18 16:05:06 +0300

    Rename NavigableEntityRepository to NavigableObjectRepository

--- src/main/java/com/iotiq/application/repository/NavigableObjectRepository.java
+++ src/main/java/com/iotiq/application/repository/NavigableObjectRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface NavigableEntityRepository extends JpaRepository<NavigableObject, UUID> {
+public interface NavigableObjectRepository extends JpaRepository<NavigableObject, UUID> {
 }
