52d7e32d698cb91539b9a35118eeede41635cf4f
Oguz Kurt <oguz@iotiq.net>
2023-09-20 18:10:45 +0300

    add an update endpoint for a navEdge

--- src/main/java/com/iotiq/application/controller/NavEdgeController.java
+++ src/main/java/com/iotiq/application/controller/NavEdgeController.java
@@ -3,6 +3,7 @@ package com.iotiq.application.controller;
 import com.iotiq.application.messages.navpoint.NavEdgeDto;
 import com.iotiq.application.service.NavEdgeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +20,13 @@ public class NavEdgeController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UUID create(@RequestBody NavEdgeDto request) {
+    public UUID create(@RequestBody @Valid NavEdgeDto request) {
         return navEdgeService.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable UUID id, @RequestBody @Valid NavEdgeDto request) {
+        navEdgeService.update(id, request);
     }
 
 }
