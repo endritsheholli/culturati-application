15e16cc423c301b0a0194b204cc1c77177d58de5
YusufZartit <gonenc@dakikyazilim.com>
2023-09-20 11:21:45 +0300

    Add 'checkIfEligibleToCreateGame' method to 'createGame'.

--- src/main/java/com/iotiq/application/repository/GameRepository.java
+++ src/main/java/com/iotiq/application/repository/GameRepository.java
@@ -1,9 +1,12 @@
 package com.iotiq.application.repository;
 
 import com.iotiq.application.domain.Game;
+import com.iotiq.application.domain.GameStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GameRepository extends JpaRepository<Game, UUID> {
+    Optional<Game> findByGameGamersGamerIdAndStatusNot(UUID gamerId, GameStatus gameStatus);
 }
