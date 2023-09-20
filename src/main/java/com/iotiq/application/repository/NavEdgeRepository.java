588fc3d35924c313e536ba5b8ccb6fd43a50d662
Oguz Kurt <oguz@iotiq.net>
2023-09-20 14:49:59 +0300

    add several methods for finding edges between two points.

--- src/main/java/com/iotiq/application/repository/NavEdgeRepository.java
+++ src/main/java/com/iotiq/application/repository/NavEdgeRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface NavEdgeRepository extends JpaRepository<NavEdge, UUID> {
+    boolean existsByStartingPointIdAndEndingPointIdAndDirectedIsTrue(UUID startingPointId, UUID endingPointId);
+    boolean existsByStartingPointIdAndEndingPointIdAndDirectedIsFalse(UUID startingPointId, UUID endingPointId);
+    boolean existsByStartingPointIdAndEndingPointId(UUID startingPointId, UUID endingPointId);
+
 }
